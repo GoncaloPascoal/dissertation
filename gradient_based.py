6539ccdeba8c653eb6e03ab841517b510b42bcaa
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2023-03-21 02:57:24 +0000

    Fully implement gradient-based optimization (QAQC Algorithm 4)

--- gradient_based.py
+++ gradient_based.py
@@ -13,10 +13,9 @@ from qiskit.quantum_info import SparsePauliOp
 from qiskit.circuit import Parameter
 from rich import print
 
-from hst import hst, lhst, cost_hst_weighted, cost_hst
+from hst import hst, lhst, cost_hst_weighted
 
 def compute_gradient(
-    qc: QuantumCircuit,
     cost_function: Callable[[np.ndarray], float],
     params: np.ndarray
 ) -> np.ndarray:
@@ -40,9 +39,9 @@ def compute_gradient(
 def gradient_based_hst_weighted(
     u: QuantumCircuit,
     v: QuantumCircuit,
-    q: float,
-    tolerance: float,
-    max_iterations: int,
+    q: float = 1.0,
+    tolerance: float = 0.01,
+    max_iterations: int = 50,
     sample_precision: float = 1.0e-3,
 ) -> Tuple[List[float], float]:
     assert 0 < tolerance < 1
@@ -50,32 +49,55 @@ def gradient_based_hst_weighted(
     assert max_iterations > 0
     assert sample_precision > 0
 
+    v_adj = v.inverse()
+
     sim = AerSimulator(shots=int(1 / sample_precision))
-    qc = transpile(hst(u, v.inverse()), sim)
-    dimensions = [(-pi, pi) for _ in range(qc.num_parameters)]
+    qc_hst = transpile(hst(u, v_adj), sim)
+    qc_lhst = transpile([lhst(u, v_adj, i) for i in range(u.num_qubits)], sim)
 
     def cost_function(params: np.ndarray) -> float:
-        qc_bound = qc.bind_parameters(params)
-        counts = sim.run(qc_bound).result().get_counts()
-        return cost_hst(counts)
+        qc_hst_bound = qc_hst.bind_parameters(params)
+        counts = sim.run(qc_hst_bound).result().get_counts()
 
-    best_params = np.array([0.0 for _ in range(qc.num_parameters)])
-    best_cost = cost_function(best_params)
+        qc_lhst_bound = [c.bind_parameters(params) for c in qc_lhst]
+        counts_list = sim.run(qc_lhst_bound).result().get_counts()
+        if isinstance(counts_list, dict):
+            counts_list = [counts_list]
+
+        return cost_hst_weighted(counts, counts_list, q)
+
+    params = np.array([0.0 for _ in range(v.num_parameters)])
+    cost = cost_function(params)
 
     i = 0
     grad_count = 0
     learning_rate = 1.0
 
     while i < max_iterations and grad_count < 4:
-        gradient = compute_gradient(qc, cost_function, best_params)
-        
+        gradient = compute_gradient(cost_function, params)
+
         gradient_norm = np.linalg.norm(gradient)
         if gradient_norm <= tolerance:
             grad_count += 1
 
+        params_1 = params - learning_rate * gradient
+        params_2 = params_1 - learning_rate * gradient
+
+        cost_2 = cost_function(params_2)
+        if cost - cost_2 >= learning_rate * gradient_norm:
+            learning_rate *= 2
+            params = params_2
+            cost = cost_2
+        else:
+            cost_1 = cost_function(params_1)
+            if cost - cost_1 < learning_rate / 2 * gradient_norm:
+                learning_rate /= 2
+            params = params_1
+            cost = cost_1
+
         i += 1
 
-    return best_params.tolist(), best_cost
+    return params.tolist(), cost
 
 u = QuantumCircuit(1)
 u.h(0)
@@ -85,6 +107,6 @@ v.rz(Parameter('a'), 0)
 v.rx(pi / 2, 0)
 v.rz(Parameter('b'), 0)
 
-best_params, best_cost = gradient_based_hst_weighted(u, v, 0.5, 0.01, 50)
+best_params, best_cost = gradient_based_hst_weighted(u, v)
 best_params_pi = [f'{p / pi:4f}π' for p in best_params]
 print(f'The best parameters were {best_params_pi} with a cost of {best_cost}.')
