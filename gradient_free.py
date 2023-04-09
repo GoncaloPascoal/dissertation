4469576d86ac6bc8c759588e069da25f396ebe5a
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2023-04-09 12:46:00 +0100

    Begin implementing gradient free bisection algorithm (QAQC Alg2)

--- gradient_free.py
+++ gradient_free.py
@@ -1,8 +1,8 @@
 
-import random
 from math import pi
-from typing import List, Optional, Tuple
+from typing import List, Optional, Sequence
 
+import numpy as np
 from qiskit_aer import AerSimulator
 from qiskit import QuantumCircuit, transpile
 from qiskit.providers.backend import Backend
@@ -10,6 +10,7 @@ import skopt
 
 from hst import HilbertSchmidt, cost_hst
 from utils import ContinuousOptimizationResult
+
 
 def gradient_free_hst(
     u: QuantumCircuit,
@@ -35,6 +36,50 @@ def gradient_free_hst(
     qc = transpile(HilbertSchmidt(u, v), sim)
     dimensions = [(-pi, pi)] * qc.num_parameters
 
+    def cost_function(params: Sequence[float]) -> float:
+        qc_bound = qc.bind_parameters(params)
+        counts = sim.run(qc_bound).result().get_counts()
+        return cost_hst(counts)
+
+    best_params = [0.0] * qc.num_parameters
+    best_cost = cost_function(best_params)
+
+    if qc.num_parameters != 0:
+        i = 0
+        while best_cost > tolerance and i < max_starting_points:
+            params = np.random.uniform(-pi, pi, qc.num_parameters).tolist()
+
+            res = skopt.gp_minimize(cost_function, dimensions, n_calls=max_iterations, x0=params)
+            params, cost = res.x, res.fun
+
+            if cost <= best_cost:
+                best_params, best_cost = params, cost
+            i += 1
+
+    return ContinuousOptimizationResult(best_params, best_cost)
+
+def gradient_free_bisection(
+    u: QuantumCircuit,
+    v: QuantumCircuit,
+    tolerance: float = 0.01,
+    max_iterations: int = 50,
+    max_bisections: int = 5,
+    sample_precision: float = 1.25e-4,
+    backend: Optional[Backend] = None,
+) -> ContinuousOptimizationResult:
+    assert 0 < tolerance < 1
+    assert max_iterations > 0
+    assert max_bisections > 0
+    assert sample_precision > 0
+
+    if backend is None:
+        sim = AerSimulator()
+    else:
+        sim = AerSimulator.from_backend(backend)
+    sim.set_option('shots', int(1 / sample_precision))
+
+    qc = transpile(HilbertSchmidt(u, v), sim)
+
     def cost_function(params: List[float]) -> float:
         qc_bound = qc.bind_parameters(params)
         counts = sim.run(qc_bound).result().get_counts()
@@ -43,17 +88,26 @@ def gradient_free_hst(
     best_params = [0.0] * qc.num_parameters
     best_cost = 1.0
 
-    i = 0
-    while best_cost > tolerance and i < max_starting_points:
-        current_params = [random.uniform(-pi, pi) for _ in range(qc.num_parameters)]
-        res = skopt.gp_minimize(cost_function, dimensions, n_calls=max_iterations, x0=current_params)
-        params, cost = res.x, res.fun
+    angles = {0, pi / 2, pi, 3 * pi / 2}
 
-        if cost <= best_cost:
-            best_params, best_cost = params, cost
-        i += 1
+    for param_idx in range(qc.num_parameters):
+        params = best_params.copy()
+
+        for angle in angles:
+            params[param_idx] = angle
+            cost = cost_function(params)
+            if cost < best_cost:
+                best_params, best_cost = params.copy(), cost
+
+    for t in range(1, max_bisections + 1):
+        for i in range(max_iterations):
+            if best_cost <= tolerance:
+                break
+
+            # TODO: Finish QAQC algorithm 2 implementation
 
     return ContinuousOptimizationResult(best_params, best_cost)
+
 
 if __name__ == '__main__':
     from qiskit.circuit import Parameter
