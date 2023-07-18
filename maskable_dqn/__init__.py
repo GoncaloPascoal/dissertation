bd835fa06e4347022d3aa9989b0fa064d0370cc3
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2023-07-18 19:43:36 +0100

    Remove MaskableDQN

--- maskable_dqn/__init__.py
+++
@@ -1,5 +0,0 @@
-
-from maskable_dqn.maskable_dqn import MaskableDQN
-from maskable_dqn.policy import MaskableDQNPolicy, MaskableMultiInputDQNPolicy
-
-__all__ = ['MaskableDQN', 'MaskableDQNPolicy', 'MaskableMultiInputDQNPolicy']
