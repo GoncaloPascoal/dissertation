427d6ceca936e8b51b751d5c9fe8d8a9fbb7bcb9
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2023-08-12 23:56:44 +0100

    Fix key for noise config

--- parsing.py
+++ parsing.py
@@ -61,7 +61,7 @@ def parse_env_config(path: str) -> Callable[[], RoutingEnv]:
         raise ValueError(f'Coupling map configuration has invalid type `{type(coupling_map_config)}`')
 
     # Can make environment noise-unaware by assigning null to noise_config
-    noise_config_args = config.pop('noise_unaware', {})
+    noise_config_args = config.pop('noise_config', {})
     noise_config = None if noise_config_args is None else NoiseConfig(**noise_config_args)
 
     obs_modules = []
