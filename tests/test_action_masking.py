220130f745a0693e3c024205c8adc608f1556738
Gonçalo Pascoal <goncalojpascoal@gmail.com>
2023-08-28 14:49:01 +0100

    Add another test

--- tests/test_action_masking.py
+++ tests/test_action_masking.py
@@ -12,6 +12,28 @@ def env_linear_4q() -> RoutingEnv:
     return RoutingEnv(linear_topology(4))
 
 
+@pytest.fixture()
+def env_linear_5q() -> RoutingEnv:
+    return RoutingEnv(linear_topology(5))
+
+
+def test_bridge(env_linear_5q: RoutingEnv):
+    env = env_linear_5q
+
+    qc = QuantumCircuit(5)
+    qc.cx(0, 2)
+    qc.cx(1, 3)
+    qc.cx(2, 4)
+
+    env.circuit = qc
+
+    obs, *_ = env.reset()
+    np.testing.assert_array_equal(obs['action_mask'], np.array([1, 1, 1, 1, 1, 1, 0]))
+
+    obs, *_ = env.step(4)
+    np.testing.assert_array_equal(obs['action_mask'], np.array([1, 1, 1, 1, 0, 1, 1]))
+
+
 def test_blocked_swap(env_linear_4q: RoutingEnv):
     env = env_linear_4q
 
